fdde6314ea89198cabcb376060ba748cd9c64b43
锅大虾 <596278576@qq.com>
2020-03-07 18:15:26 +0800

    '购物车页面功能实现'

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -5,22 +5,90 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    address:{},
+    list:wx.getStorageSync('goods')||[],
+    allSelect:'',
+    allPrice:''
   },
   //获取收货地址按钮
   handleshdz(){
     wx.chooseAddress({
       success:(res)=>{
         console.log(res)
+        this.setData({
+          address:{
+            provinceName: res.provinceName,
+            cityName: res.cityName,
+            countyName: res.countyName,
+            userName: res.userName,
+            telNumber: res.telNumber
+          }
+        })
+        wx.setStorageSync('address', this.data.address)
       }
     })
   },
+  //计算价格
+  handelPrice(){
+    let allPrice = 0
+    this.data.list.forEach(v => {
+      if(v.select){
+        allPrice += v.goods_price * v.number
+      }
+    })
+    this.setData({
+      allPrice,
+      list:this.data.list
+    })
+    wx.setStorageSync('goods',this.data.list)
+  },
+  handelNum(e){
+    console.log(e)
+    let { index, num } = e.currentTarget.dataset
+    this.data.list[index].number += num
+    this.handelPrice()
+  },
+  handelBlur(e){
+    let {index} = e.currentTarget.dataset
+    let {value}=e.detail
+    this.data.list[index].number=+value
+    this.handelPrice()
+  },
+  handelSelect(e){
+    let { index } = e.currentTarget.dataset
+    this.data.list[index].select = !this.data.list[index].select
+    this.handelPrice()
 
+    let allSelect = this.data.list.some(v => {
+      return !v.select
+    })
+    allSelect = !allSelect
+    this.setData({
+      allSelect
+    })
+  },
+  handelAllselect(){
+    this.data.allSelect = !this.data.allSelect
+    this.data.list.forEach(v=>{
+      v.select = this.data.allSelect
+    })
+    this.handelPrice()
+    this.setData({
+      allSelect:this.data.allSelect
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.getStorage({
+      key: 'address',
+      success:(res)=> {
+        this.setData({
+          address: res.data
+        })
+      }
+    })
   },
 
   /**
@@ -34,8 +102,17 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
-  },
+    let arr = wx.getStorageSync('goods') || []
+    let allSelect=this.data.list.some(v=>{
+      return !v.select
+    })
+    allSelect = !allSelect
+    this.setData({
+      list:arr,
+      allSelect
+    })
+    this.handelPrice()
+  },  
 
   /**
    * 生命周期函数--监听页面隐藏
--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,3 +1,4 @@
+import request from '../../request/index.js'
 // pages/goods_detail/index.js
 Page({
 
@@ -5,62 +6,122 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    list: [],
+    bigList: [],
+    current: 0,
+    goods: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
+    // console.log(options)
+    request({
+      url: '/goods/detail',
+      data: {
+        goods_id: options.goods_id
+      }
+    }).then(res => {
+      console.log(res)
+      let arr = res.data.message.pics.map(v => {
+        return v.pics_big
+      })
+      this.setData({
+        list: res.data.message,
+        bigList: arr
+      })
 
+    })
+
+    this.data.goods = wx.getStorageSync('goods') || []
+    console.log(this.data.goods)
+  },
+  handelTab(e) {
+    // console.log(e)
+    this.setData({
+      current: e.currentTarget.dataset.index
+    })
+  },
+  handelBig(e) {
+    console.log(123)
+    wx.previewImage({
+      current: this.data.bigList[e.currentTarget.dataset.index], // 当前显示图片的http链接
+      urls: this.data.bigList // 需要预览的图片http链接列表
+    })
   },
 
+  handeladd() {
+    let res = this.data.goods.some(v => {
+      if (v.goods_id === this.data.list.goods_id) {
+        v.number++
+      }
+      return v.goods_id === this.data.list.goods_id
+    })
+    if (!res) {
+      this.data.goods.unshift({
+        goods_id: this.data.list.goods_id,
+        goods_name: this.data.list.goods_name,
+        goods_price: this.data.list.goods_price,
+        goods_small_logo: this.data.list.goods_small_logo,
+        number: 1,
+        select:true
+      })
+    }
+    wx.setStorageSync('goods', this.data.goods)
+    // console.log(this.data.goods)
+    wx.showToast({
+      title: '成功',
+      icon: 'success',
+      duration: 2000
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
